19aeef54de33206b0f3e699c1c3e50975842d7e7
sweet-nightmare <150767583+sweet-nightmare@users.noreply.github.com>
2024-01-05 11:39:38 +0700

    Update NodeSender.cpp

--- NodeSender.cpp
+++ NodeSender.cpp
@@ -1,67 +1,49 @@
+#include <Arduino.h>
 #include <ESP8266WiFi.h>
-#include <ESPNowW.h>
-#include <TaskScheduler.h>
-
-Scheduler taskScheduler;
+#include <espnow.h>
 
 const int sensorPin = D2;
 const int buzzerPin = D1;
 
-uint8_t broadcastAddress[] = {0x48, 0x55, 0x19, 0xF0, 0xC7, 0x9B};
+uint8_t receiverAddress[] = {0x48, 0x55, 0x19, 0xf0, 0xc7, 0x9b};
 
 struct struct_message {
-   int sensorValue;
+  int sensorValue;
 };
 struct_message dataToSend;
 
 void OnDataSent(uint8_t *mac_addr, uint8_t sendStatus) {
   Serial.print("Last Packet Send Status: ");
-  if (sendStatus == 0) {
+  if (sendStatus == 1) {
     Serial.println("Pengiriman sukses");
   } else {
     Serial.println("Pengiriman Gagal");
   }
 }
 
-
-void readSensor(); // Declare readSensor function
-
-Task taskSensor(100, TASK_FOREVER, &readSensor);
-
 void readSensor() {
-  // Baca sensor MC38
   int sensorValue = digitalRead(sensorPin);
-
-  // Buat objek struct_message dan isi nilai sensor
   dataToSend.sensorValue = sensorValue;
 
-  // Kirim data melalui ESPNow
-  esp_now_send(broadcastAddress, (uint8_t*)&dataToSend, sizeof(dataToSend));
+  esp_now_send(receiverAddress, (uint8_t*)&dataToSend, sizeof(dataToSend));
 
-  // Cek nilai sensor
   if (sensorValue == 1) {
-    digitalWrite(buzzerPin,HIGH); //hidupkan buzzer
+    digitalWrite(buzzerPin, HIGH);
     delay(100);
-    digitalWrite(buzzerPin,LOW); //matikan buzzer
+    digitalWrite(buzzerPin, LOW);
     delay(100);
   } else {
-    // Jika nilai sensor adalah 1, aktifkan buzzer
-    digitalWrite(buzzerPin,LOW); // matikan buzzer
+    digitalWrite(buzzerPin, LOW);
   }
 
-  // Lakukan sesuatu dengan nilai sensor, contoh: cetak nilai
   Serial.print("Sensor Value: ");
   Serial.println(sensorValue);
 }
 
 void setup() {
-  // Init Serial Monitor
   Serial.begin(115200);
-
-  // Set device as a Wi-Fi Station
   WiFi.mode(WIFI_STA);
 
-  // Init ESP-NOW
   if (esp_now_init() != 0) {
     Serial.println("Error initializing ESP-NOW");
     return;
@@ -70,20 +52,16 @@ void setup() {
   esp_now_set_self_role(ESP_NOW_ROLE_CONTROLLER);
   esp_now_register_send_cb(OnDataSent);
 
-  // Register peer
-  esp_now_add_peer(broadcastAddress, ESP_NOW_ROLE_SLAVE, 1, NULL, 0);
+  if (esp_now_add_peer(receiverAddress, ESP_NOW_ROLE_SLAVE, 1, NULL, 0) != 0) {
+    Serial.println("Failed to add peer");
+  } else {
+    Serial.println("Peer added");
+  }
 
   pinMode(sensorPin, INPUT_PULLUP);
   pinMode(buzzerPin, OUTPUT);
-
-  taskScheduler.init();
-  taskScheduler.addTask(taskSensor);
-  taskSensor.enable();
-  taskScheduler.startNow();
-  readSensor();
 }
 
 void loop() {
-  // Eksekusi tugas pada jadwal yang ditentukan oleh scheduler
-  taskScheduler.execute();
+  readSensor();
 }
